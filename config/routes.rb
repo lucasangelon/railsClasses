dcc4ab8daf610a2739f6d8d6868e5117ea8c4f3a
Lucas Angelon <lucas.a.arouca@gmail.com>
2017-02-28 22:03:43 +0800

    chore(rails): add application template

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'friends/ross'
+
+  get 'friends/monica'
+
+  get 'friends/rachel'
+
+  get 'friends/phoebe'
+
+  get 'friends/chandler'
+
+  get 'friends/joey'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
