c438c276242dd7ba6534b7d0bd1ba832dd1473a3
Lucas Angelon <lucas.a.arouca@gmail.com>
2017-02-28 22:17:05 +0800

    chore(friends): add sample data

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -9,21 +9,41 @@ class FriendsController < ApplicationController
 
   def monica
     @friend = Friend.new(
-        first_name: 'Ross',
+        first_name: 'Monica',
         last_name: 'Geller',
-        career: 'Archaeologist',
-        famous_sentence: 'We were on a break!')
+        career: 'Chef',
+        famous_sentence: 'I know!')
   end
 
   def rachel
+    @friend = Friend.new(
+        first_name: 'Rachel',
+        last_name: 'Green',
+        career: 'Fashion',
+        famous_sentence: 'I got off the plane.')
   end
 
   def phoebe
+    @friend = Friend.new(
+        first_name: 'Phoebe',
+        last_name: 'Buffay',
+        career: 'Musician',
+        famous_sentence: 'My eyes!')
   end
 
   def chandler
+    @friend = Friend.new(
+        first_name: 'Chandler',
+        last_name: 'Bing',
+        career: 'Transponster',
+        famous_sentence: 'Can you Be <insert something here>')
   end
 
   def joey
+    @friend = Friend.new(
+        first_name: 'Joey',
+        last_name: 'Tribbiani',
+        career: 'Actor',
+        famous_sentence: 'How you doin?')
   end
 end
