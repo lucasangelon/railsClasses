0b24b8d9fb2921f4dd94cfc30c95ed41df99d969
Lucas Angelon <lucas.a.arouca@gmail.com>
2017-02-28 22:24:15 +0800

    fix(friends-controller): use normal initialize

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,49 +1,25 @@
 class FriendsController < ApplicationController
   def ross
-    @friend = Friend.new(
-        first_name: 'Ross',
-        last_name: 'Geller',
-        career: 'Archaeologist',
-        famous_sentence: 'We were on a break!')
+    @friend = Friend.new('Ross', 'Geller', 'Archaeologist', 'We were on a break!')
   end
 
   def monica
-    @friend = Friend.new(
-        first_name: 'Monica',
-        last_name: 'Geller',
-        career: 'Chef',
-        famous_sentence: 'I know!')
+    @friend = Friend.new('Monica', 'Geller', 'Chef', 'I know!')
   end
 
   def rachel
-    @friend = Friend.new(
-        first_name: 'Rachel',
-        last_name: 'Green',
-        career: 'Fashion',
-        famous_sentence: 'I got off the plane.')
+    @friend = Friend.new('Rachel', 'Green', 'Fashion', 'I got off the plane.')
   end
 
   def phoebe
-    @friend = Friend.new(
-        first_name: 'Phoebe',
-        last_name: 'Buffay',
-        career: 'Musician',
-        famous_sentence: 'My eyes!')
+    @friend = Friend.new('Phoebe', 'Buffay', 'Musician', 'My eyes!')
   end
 
   def chandler
-    @friend = Friend.new(
-        first_name: 'Chandler',
-        last_name: 'Bing',
-        career: 'Transponster',
-        famous_sentence: 'Can you Be <insert something here>')
+    @friend = Friend.new('Chandler', 'Bing', 'Transponster', 'Can you Be <insert something here>')
   end
 
   def joey
-    @friend = Friend.new(
-        first_name: 'Joey',
-        last_name: 'Tribbiani',
-        career: 'Actor',
-        famous_sentence: 'How you doin?')
+    @friend = Friend.new('Joey', 'Tribbiani', 'Actor', 'How you doin?')
   end
 end
