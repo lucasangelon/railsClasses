7530643875b047bdc5e7ff4da8eb6b6ae74ec8d4
Lucas Angelon <lucas.a.arouca@gmail.com>
2017-02-28 22:08:20 +0800

    feat(friend): add model

---
+++ app/models/friend.rb
@@ -0,0 +1,10 @@
+class Friend
+  attr_accessor :first_name, :last_name, :career, :famous_sentence
+
+  def initialize(first_name, last_name, career, famous_sentence)
+    @first_name = first_name
+    @last_name = last_name
+    @career = career
+    @famous_sentence = famous_sentence
+  end
+end
