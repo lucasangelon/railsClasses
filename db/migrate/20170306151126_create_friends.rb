d2e03ed909ac8529dcabd449335f5265c53a8110
Lucas Angelon <lucas.a.arouca@gmail.com>
2017-03-07 00:18:32 +0800

    docs(create-friends): add comments to migration

--- db/migrate/20170306151126_create_friends.rb
+++ db/migrate/20170306151126_create_friends.rb
@@ -1,4 +1,11 @@
 class CreateFriends < ActiveRecord::Migration[5.0]
+
+  # This will execute the migration for us.
+  # We used to have Up and Down methods before, we can still use them
+  # but Rails can usually guess what it needs to do to rollback on its own.
+  #
+  # Obviously there are going to be exceptions to this so it is important to
+  # know that you _can_ define your own up / down methods.
   def change
     create_table :friends do |t|
       t.string :first_name
@@ -6,7 +13,29 @@ class CreateFriends < ActiveRecord::Migration[5.0]
       t.string :career
       t.string :famous_sentence
 
+      # Generates create_at and updated_at timestamps for us.
       t.timestamps
     end
   end
+
+  # This was the way to do it before, now we only use it in specific cases.
+  #
+  # def up
+  #   create_table :friends do |t|
+  #     t.string :first_name
+  #     t.string :last_name
+  #     t.string :career
+  #     t.string :famous_sentence
+  #
+  #     t.timestamps
+  #   end
+  # end
+
+  # This is dangerous. If we rollback, we will get rid of this column.
+  # But when we try to go back up, we'll have issues as the table will already exist.
+  # Be careful when manually executing migrations.
+  #
+  # def down
+  #   remove_column :friends, :career, :string
+  # end
 end
